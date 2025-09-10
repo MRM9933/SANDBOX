9dd2d0f27911496aec3953f1e5177a6d8fd1d5c8
MRM9933 <74165184+MRM9933@users.noreply.github.com>
2025-09-10 17:07:52 +0000

    more changes

---
+++ salaryalgo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+
+double hrs;
+double sal;
+double rte = 7.50;
+
+cout<< " How many hours did you work?";
+cin >> hrs;
+
+if(hrs <=40){
+    sal = hrs*rte;
+}
+else{
+    sal = 40*rte+(hrs-40)*rte*1.5;
+}
+
+cout<< " your calculated salary: "<< sal<< endl;
+
+
+
+return 0;
+
+}
